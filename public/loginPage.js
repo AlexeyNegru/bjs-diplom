4e500c20306006a5ee075b9327cc2f1a2d65f640
Alex n <Shakal240@gmail.com>
2023-04-09 15:40:50 +0300

    Логин готов

---
+++ public/loginPage.js
@@ -0,0 +1,25 @@
+"use strict"
+const userForm = new userForm();
+userForm.loginFormCallback = (data) => {
+    ApiConnector.login(data, response => {
+        if (response.success) {
+            location.reload();
+            return
+        } else {
+            console.log(response.error);
+            userForm.setLoginErrorMessage(response.error);
+        }
+    })
+}
+
+userForm.registerFormCallback = (data) => {
+    ApiConnector.register(data, response => {
+        if (response.success) {
+            location.reload();
+            return
+        } else {
+            console.log(response.error);
+            userForm.setLoginErrorMessage(response.error); 
+        }
+    })
+}
